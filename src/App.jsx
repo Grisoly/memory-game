9fe862417cfc4e2136c3e01b5dae4ed35eaeb2c9
Grisoly <120173770+Grisoly@users.noreply.github.com>
2023-12-12 22:42:35 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+const cardImages = [
+  {"src": "/img/kiyotaka_ijichi.png"},
+  {"src": "/img/maki_zenin.png"},
+  {"src": "/img/masamichi_yaga.png"},
+  {"src": "/img/megumi_fushiguro.png"},
+  {"src": "/img/nobara_kugisaki.png"},
+  {"src": "/img/ryomen_sukuna.png"}
+]
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Beany_off Magic Match</h1>
+      <button>New Game</button>
+      </div>
+  )
+}
+
+export default App
